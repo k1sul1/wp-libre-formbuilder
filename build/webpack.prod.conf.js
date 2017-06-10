ade050b6744db106abd9652e964ba46983d1c884
Christian Nikkanen <hi@kisu.li>
2017-06-10 21:39:03 +0300

    Use version from package.json

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,3 +1,4 @@
+var pjson = require('../package.json');
 var path = require('path')
 var utils = require('./utils')
 var webpack = require('webpack')
@@ -23,8 +24,8 @@ var webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    filename: utils.assetsPath('js/[name].' + pjson.version + '.js'),
+    chunkFilename: utils.assetsPath('js/[id].' + pjson.version + '.js')
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
@@ -39,7 +40,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.assetsPath('css/[name].' + pjson.version + '.css')
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
