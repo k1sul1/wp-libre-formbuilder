105e99e7f94012118ebb92b5c74ba8bc4758b220
Christian Nikkanen <hi@kisu.li>
2017-06-15 12:41:04 +0200

    Add Vuex boilerplate

--- src/main.js
+++ src/main.js
@@ -1,22 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+// import Vuex from 'vuex';
 import App from './App';
 import router from './router';
-// import draggable from 'vuedraggable';
 
 Vue.config.productionTip = false;
-
-// Vue.component('wplfb-field', {
-  // components: { draggable },
-  // render: ,
-  // props:
-// });
+// Vue.use(Vuex);
 
 /* eslint-disable no-new */
+/* const store = new Vuex.Store({
+  state: {
+    tree: {}
+  },
+
+  mutations: {
+    updateTree (state) {
+
+    }
+  }
+}); */
+
 new Vue({
   el: '#wplfb_buildarea',
   router,
+  // store,
   template: '<App/>',
   components: { App }
 });
