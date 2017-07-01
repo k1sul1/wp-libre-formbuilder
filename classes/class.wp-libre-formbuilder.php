1ebf269e7844c12c6238696b23a20054736233c5
Christian Nikkanen <hi@kisu.li>
2017-07-01 08:41:19 +0300

    Add REST routes

--- classes/class.wp-libre-formbuilder.php
+++ classes/class.wp-libre-formbuilder.php
@@ -1,6 +1,9 @@
 <?php
 
 class WP_Libre_Formbuilder {
+  const ERR_FORM_ID_EMPTY = 'You must supply a form id.';
+  const FORM_SAVED = 'Form saved succesfully.';
+
   public static $instance;
 
   public static function instance() {
@@ -23,6 +26,7 @@ class WP_Libre_Formbuilder {
     });
 
     add_action("add_meta_boxes", [$this, "tamperMetaBoxes"]);
+    add_action("rest_api_init", [$this, "registerRESTRoutes"]);
   }
 
   public function registerCPT() {
@@ -72,5 +76,86 @@ class WP_Libre_Formbuilder {
       "advanced",
       "high"
     );
+  }
+
+  public function registerRESTRoutes() {
+    register_rest_route("wplfb", "/forms/form", [
+      "methods" => "GET",
+      "callback" => function (WP_REST_Request $request) {
+        return $this->getForm($request);
+      },
+    ]);
+
+    register_rest_route("wplfb", "/forms/form", [
+      "methods" => "POST",
+      "callback" => function (WP_REST_Request $request) {
+        return $this->saveForm($request);
+      },
+    ]);
+  }
+
+  public function getForm(WP_REST_Request $request) {
+    $form_id = 55; // Temp.
+    if (is_null($form_id)) {
+      return new WP_REST_Response([
+        "error" => self::ERR_FORM_ID_EMPTY
+      ]);
+    }
+
+    $p = get_post($form_id);
+
+    return new WP_REST_Response([
+      "post" => $p,
+      "fields" => get_post_meta($p->ID, "wplfb_fields", true)
+    ]);
+  }
+
+  public function saveForm(WP_REST_Request $request) {
+    $form_id = 55; // Temp.
+
+    // Do not check for null. Create a new one.
+    // if (is_null($form_id)) {
+      // return new WP_REST_Response([
+        // "error" => self::ERR_FORM_ID_EMPTY
+      // ]);
+    // }
+
+    $params = $request->get_body_params();
+    $fields = $params["fields"];
+
+
+    update_post_meta($form_id, "wplfb_fields", $fields); // Sanitize?
+    $insert = wp_insert_post([
+      "ID" => !is_null($form_id) ? $form_id : 0,
+      "post_content" => $this->generateHTML($fields)
+    ]);
+
+    if (!is_wp_error($insert) && $insert !== 0) {
+      return new WP_REST_Response([
+        "success" => self::FORM_SAVED,
+        "fields" => $fields
+      ]);
+    } else {
+      return new WP_REST_Response([
+        "error" => $insert
+      ]);
+    }
+
+  }
+
+  public function generateHTML($json = '') {
+    $obj = json_decode($json);
+    $shorts = ["br", "img"]; // Add all.
+    $html = "";
+
+    foreach ($obj as $key => $value) {
+      $is_short = in_array($key, $shorts);
+      $mayClose = $is_short ? "/" : "";
+      $html .= "<$key $attrs $mayClose>";
+      $html .= ""; // Add children here.
+      $html .= !$is_short ? "</$key>" : "";
+    }
+
+    return $html;
   }
 }
