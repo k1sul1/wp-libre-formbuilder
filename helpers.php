5633c92e79686b0bd2f8908fc161cd06f3d970b1
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-08-19 19:46:39 +0300

    I hate PHP.

--- helpers.php
+++ helpers.php
@@ -9,29 +9,7 @@ namespace WPLFB;
  * @throws
  */
 function booleanify($data) {
-  error_log($data);
-  if (is_bool($data)) {
-    return $data;
-  } else if (is_string($data)) {
-    switch (strtolower($data)) {
-      case "true":
-        return true;
-      break;
-
-      case "false":
-        return false;
-      break;
-    }
-  } else if (is_numeric($data)) {
-    // For once typecasting is good for something.
-    if ($data == 0) {
-      return false;
-    } else if ($data == 1) {
-      return true;
-    }
-  }
-
-  throw new \Exception("Unable to transform $data into boolean");
+  return (bool) $data;
 }
 
 
