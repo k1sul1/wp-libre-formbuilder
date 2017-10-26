b83743ff85700543adbd099601e01d51accc87ea
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-10-26 08:39:43 +0300

    Add HMR capability

--- rsrc/src/index.js
+++ rsrc/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+const root = document.getElementById('root');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, root);
 registerServiceWorker();
+
+// Doesn't really work when working with React state, switch to redux first.
+// if (module.hot) {
+  // module.hot.accept('./App', () => {
+    // ReactDOM.render(
+      // <App />,
+      // root
+    // );
+  // });
+// }
