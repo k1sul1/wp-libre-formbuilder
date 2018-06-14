0086e4e1202b9420d225113b27c500594cde8590
Christian Nikkanen <christian.nikkanen@vincit.com>
2018-06-14 20:22:49 +0300

    Refactor data structure, remove useless shit

--- classes/class.wp-libre-formbuilder.php
+++ classes/class.wp-libre-formbuilder.php
@@ -29,7 +29,10 @@ class WP_Libre_Formbuilder {
     add_action("save_post", function($post_id, $post) {
       if ($post->post_type === "wplfb-field") {
         $children = !isset($_POST["wplfb-field-children"]) ? 0 : $_POST["wplfb-field-children"];
+        $template = !empty($_POST["wplfb-field-template"]) ? $_POST["wplfb-field-template"] : "";
+
         update_post_meta($post_id, "wplfb-field-children", $children);
+        update_post_meta($post_id, "wplfb-field-template", $template);
       }
     }, 10, 2);
 
@@ -65,6 +68,11 @@ class WP_Libre_Formbuilder {
       "Field options",
       function($post) {
       ?>
+      <label>
+        <strong>Field template</strong>
+        <textarea name="wplfb-field-template"><?=get_post_meta($post->ID, "wplfb-field-template", true)?></textarea>
+      </label><br>
+
       <label>
       <input type="checkbox" name="wplfb-field-children" value="1" <?=checked(1, get_post_meta($post->ID, "wplfb-field-children", true))?>>
         Field accepts children
@@ -203,7 +211,6 @@ class WP_Libre_Formbuilder {
         "error" => $insert
       ]);
     }
-
   }
 
   public function getField(WP_REST_Request $request) {
@@ -220,8 +227,9 @@ class WP_Libre_Formbuilder {
       $ok = $this->addField([
         "key" => $p->ID,
         "name" => $p->post_title,
-        "html" => $p->post_content,
-        "takesChildren" => \WPLFB\booleanify(get_post_meta($p->ID, "wplfb-field-children", true)),
+        "field" => $p->post_content,
+        "template" => get_post_meta($p->ID, "wplfb-field-template", true),
+        // "takesChildren" => \WPLFB\booleanify(get_post_meta($p->ID, "wplfb-field-children", true)),
       ]);
 
       if (!$ok) {
@@ -230,7 +238,6 @@ class WP_Libre_Formbuilder {
         update_post_meta($p->ID, "wplfb-field-override", true);
       } else {
         // I'd love to store the DOM in meta ($ok["dom"]) but I do not want to do it every time.
-
       }
     }
 
@@ -249,10 +256,10 @@ class WP_Libre_Formbuilder {
       throw new Exception("Field key is mandatory. Numerical keys *will* override database entries.");
     } else if (empty($data["name"])) {
       throw new Exception("Field name is mandatory");
-    } else if (empty($data["html"])) {
-      throw new Exception("Field html is mandatory");
-    } else if (!isset($data["takesChildren"])) {
-      throw new Exception("You must spesify whether the field takes children");
+    } else if (empty($data["field"])) {
+      throw new Exception("Field is mandatory (html)");
+    } else if (!isset($data["template"])) {
+      throw new Exception("Field template is mandatory");
     }
 
     if (!empty($this->fields[$data["key"]])) {
@@ -261,96 +268,10 @@ class WP_Libre_Formbuilder {
 
     $this->fields[$data["key"]] = [
       "name" => apply_filters("wplfb-field-name", $data["name"], $data),
-      "html" => apply_filters("wplfb-field-html", $data["html"], $data), // Is this any good? Could be for something, not for this directly
-      "dom" => apply_filters("wplfb-field-dom", $this->generateDOM($data["html"]), $data),
-      "takesChildren" => apply_filters("wplfb-field-children", \WPLFB\booleanify($data["takesChildren"]), $data), // this isn't useful, remove
-      "wplfbKey" => $data["key"], // this is a duplicate, get rid of it
+      "field" => apply_filters("wplfb-field-field", $data["field"], $data), // Is this any good? Could be for something, not for this directly
+      "template" => apply_filters("wplfb-field-template", $data["template"], $data), // Is this any good? Could be for something, not for this directly
     ]; // PHP casts it into an array if it's null.
 
     return $this->fields[$data["key"]];
-  }
-
-  public function generateDOM($html = '') {
-    $DOM = new DOMDocument();
-    $DOM->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-
-    $parseNode = function($node, $parseNode) use ($DOM) { // We have to pass the function as the second param or it isn't in scope.
-      $parseAttrs = function($el){
-        $attrs = false;
-
-        if ($el->attributes) {
-          $attrs = [];
-          foreach ($el->attributes as $attr) {
-            // `class` and `for` attributes cause trouble in React, rewrite them *there*, not here.
-            // if (strtolower($attr->name) === "class") {
-              // $attrs[$attr->name] = $attr->value;
-            // } else {
-              $attrs[$attr->name] = $attr->value;
-            // }
-          }
-        }
-
-        return $attrs;
-      };
-
-
-      return [
-        "element" => !empty($node->tagName) ? $node->tagName : "TextNode",
-        "textContent" => !empty($node->textContent) ? $node->textContent : false,
-        "attributes" => $parseAttrs($node),
-        "children" => $node->firstChild ? $parseNode($node->firstChild, $parseNode) : false, // And again. Gotta love PHP?
-        "children_html" => $node->firstChild ? $DOM->saveHTML($node->firstChild) : false,
-      ];
-    };
-
-    return $parseNode($DOM->firstChild, $parseNode); // <3
-  }
-
-  public function generateHTML($fields) {
-    $fields = (array) $fields;
-
-    $html = "";
-    $chunks = [];
-    $dom = new DOMDocument();
-    $generateChunk = function ($id, $field) use (&$dom, &$chunks) {
-      if (!empty($chunks[$id])) {
-        return $chunks[$id];
-      }
-
-      // WTF, what do you mean tagName doesn't exist sometimes?
-      // I fucking hate PHP.
-      $tagName = $field->field->tagName ?? "mark";
-      $attributes = !empty($field->field->attributes)
-        ? $field->field->attributes
-        : [];
-      $element = $dom->createElement($tagName);
-
-      foreach ($attributes as $key => $value) {
-        $element->setAttribute($key, $value);
-      }
-
-      foreach ($field->children as $child) {
-        $element->appendChild($chunks[$child]);
-      }
-
-      $chunks[$id] = $element;
-      return $chunks[$id];
-    };
-
-
-    foreach ($fields as $id => $field) {
-      if (!empty($field->children)) {
-        foreach ($field->children as $child_id) {
-          $generateChunk($child_id, $fields[$child_id]);
-        }
-      }
-
-      if (empty($field->parent)) {
-        $html .= $dom->saveHTML($generateChunk($id, $field));
-      }
-    }
-
-    error_log(print_r($html, true));
-    return $html;
   }
 }
